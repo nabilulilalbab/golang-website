60d1112862e93dccde4a796270f68f99e1b198ac
nabilulilalbab <nabilulilalbab78@gmail.com>
2025-01-17 15:29:23 +0700

    commit 11

--- header_test.go
+++ header_test.go
@@ -8,33 +8,66 @@ import (
 	"testing"
 )
 
-
-func RequestHeader(w http.ResponseWriter, r *http.Request)  {
-  contentType := r.Header.Get("content-type")
-  fmt.Fprint(w,contentType)
+func RequestHeader(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("content-type")
+	fmt.Fprint(w, contentType)
 }
 
-func TestRequestHeader(t *testing.T)  {
-  request := httptest.NewRequest(http.MethodPost, "http://localhost",nil)
-  request.Header.Add("content-type","application/json")
-  recorder := httptest.NewRecorder()
-  RequestHeader(recorder,request)
-  response := recorder.Result()
-  body, _ := io.ReadAll(response.Body)
-  fmt.Println(string(body))
+func TestRequestHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost", nil)
+	request.Header.Add("content-type", "application/json")
+	recorder := httptest.NewRecorder()
+	RequestHeader(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
 }
 
-
-func ResponseHeader(w http.ResponseWriter, r *http.Request)  {
-  w.Header().Add("x-powered-by","nabil ulil albab")
-  fmt.Fprint(w, "ok")
+func ResponseHeader(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("x-powered-by", "nabil ulil albab")
+	fmt.Fprint(w, "ok")
 }
-func TestResponseHeader(t *testing.T)  {
-  request := httptest.NewRequest(http.MethodPost,"http://localhost",nil)
-  recorder := httptest.NewRecorder()
-  ResponseHeader(recorder,request)
-  response := recorder.Result()
-  body, _ := io.ReadAll(response.Body)
-  fmt.Println(string(body))
-  fmt.Println(response.Header.Get("x-powered-by"))
+
+func TestResponseHeader(t *testing.T) {
+	request := httptest.NewRequest(http.MethodPost, "http://localhost", nil)
+	recorder := httptest.NewRecorder()
+	ResponseHeader(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+	fmt.Println(response.Header.Get("x-powered-by"))
+}
+
+// Client Request:
+// [Request Headers] --> Server
+//
+// Server Response:
+// Server --> [Response Headers] --> Client
+//
+// Request Header:
+//
+// Dikirim oleh client ke server
+// Berisi informasi tentang request
+// Contoh: Content-Type, Authorization, Accept
+// Dibaca di sisi server menggunakan r.Header
+// Response Header:
+//
+// Dikirim oleh server ke client
+// Berisi metadata tentang response
+// Contoh: Content-Type, X-Powered-By, Cache-Control
+// Ditulis di sisi server menggunakan w.Header()
+
+func RequestHeader2(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("content-type")
+	fmt.Fprint(w, contentType)
+}
+
+func TestRequestHeader2(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost", nil)
+	recorder := httptest.NewRecorder()
+	request.Header.Add("content-type", "application/json")
+	RequestHeader2(recorder, request)
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
 }
